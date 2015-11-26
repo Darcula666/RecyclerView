96e2c74dd99fd77165a20e2b7750447424fcd8fa
xiaobu <1172504039@qq.com>
2015-11-26 13:34:48 +0800

    改变布局

--- app/src/main/java/net/gfdz/com/recyclerview/MainActivity.java
+++ app/src/main/java/net/gfdz/com/recyclerview/MainActivity.java
@@ -2,8 +2,12 @@ package net.gfdz.com.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         //设置ReycilerView的布局管理
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
         //设置ReycilerView的item间分割线
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+       // mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
 
     }
 
@@ -39,5 +43,31 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 'A'; i <= 'z'; i++) {
             mDatas.add(""+(char)i);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+      switch (id){
+          case R.id.action_listView:
+              mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+              break;
+          case R.id.action_gridView:
+              mRecyclerView.setLayoutManager(new GridLayoutManager(this,3));
+              break;
+          case R.id.action_hor_gridView:
+              mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.HORIZONTAL));
+              break;
+          case R.id.action_staggered:
+              break;
+
+      }
+        return super.onOptionsItemSelected(item);
     }
 }
